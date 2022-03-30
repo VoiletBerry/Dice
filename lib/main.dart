574b3e0772008915af2d5cb345241f0d02b14e4d
Salman Ejaz <102544662+VoiletBerry@users.noreply.github.com>
2022-03-30 19:38:47 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldn = 1; //left dice number
+  int rdn = 1; //right dice number
+
+  void fun() {
+    setState(() {
+      ldn = Random().nextInt(6) + 1;
+      rdn = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  fun();
+                },
+                child: Image.asset('images/dice$ldn.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  fun();
+                },
+                child: Image.asset('images/dice$rdn.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
